d9b151a8464bcd71223aece57d4ec8a14c55a4c4
Majdolen7 <frau.haidar@gmail.com>
2026-05-13 19:53:27 +0200

    Initial academic website setup

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://majdolenhaidar.github.io',
+  base: '/',
+  integrations: [sitemap()],
+  output: 'static',
+});
